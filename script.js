0740fd5a5dd066240cde6ecbf204b16d36066326
Kelly Born <kellyaborn@gmail.com>
2020-04-30 10:12:06 -0400

    final commit lab3

--- script.js
+++ script.js
@@ -10,27 +10,30 @@ let scores = [
 
 //2. Declare a function named addScore
 const addScore = (array, newName, newScore, newDate) => {
-    let newObject = { name: newName, score: newScore, date: newDate, passed: newScore >= 60 };
+    let newObject = { name: newName, score: newScore, date: newDate, passed: score >= 60 };
     array.push(newObject);
 }
-addScore(scores, "Jake", 55, "2020-02-14");
-console.log(scores);
+//addScore(scores, "Jake", 55, "2020-02-14");
+//console.log(scores);
 
 
 //3. Declare a function named deleteScoreByIndex
+//index is position, 1 is how many to delete
 const deleteScoreByIndex = (array, index) => {
-    array.splice(index, 1);
+    array.splice(index, 1)
 }
 // deleteScoreByIndex(scores, 0);
-// console.log(scores);
+// console.log(scores)
 
 //4. Declare a function named deleteScoreByName
 const deleteScoreByName = (array, name) => {
-    const index = array.findIndex(person => {
-        return person.name === name;
+    let index = array.findIndex(person => {
+        return person.name === name
     });
     array.splice(index, 1);
 }
+//deleteScoreByName(scores, "Jane")
+//console.log(scores)
 
 //5. Declare a function named editScore
 const editScore = (array, index, score) => {
@@ -46,6 +49,16 @@ const editScore = (array, index, score) => {
 // editScore(scores, 0, 40);
 // console.log(scores);
 
+//Another option
+// const editScore = (array, index, score) => {
+//     array[index].score = score;
+//     array[index].passed = score >= 60;
+// }
+
+//editScore(scores, 2, 99)
+//console.log(scores)
+
+
 //change an object
 let human = {
     name: "Bob",
@@ -57,23 +70,24 @@ human.age = 7;
 
 //6. Declare a function named findScoreByName 
 const findScoreByName = (array, name) => {
-    return array.find((score) => {
-        return score.name === name;
-    });
-};
-console.log(findScoreByName(scores, "Jill")); //returns an object
+    let foundPerson = array.find(person => {
+        return person.name === name;
+    })
+    return foundPerson;
+}
+//console.log(findScoreByName(scores, "Jill")) //returns an object
 
 //7. Declare a function named findLowestScore
 const findLowestScore = (array) => {
     let currentlowestScore = array[0];
-    array.forEach((score) => {
-        if (score.score < currentlowestScore.score) {
-            currentlowestScore = score;
+    array.forEach(person => {
+        if (person.score < currentlowestScore.score) {
+            currentlowestScore = person;
         }
     })
-    return currentlowestScore;
-};
-console.log(findLowestScore(scores));
+    return currentlowestScore.score;
+}
+//console.log(findLowestScore(scores));
 
 //8. Declare a function named findAverageQuizScore
 const findAverageQuizScore = (array) => {
@@ -83,21 +97,35 @@ const findAverageQuizScore = (array) => {
     }
     return currentScores / array.length;
 }
-console.log(findAverageQuizScore(scores));
+//console.log(findAverageQuizScore(scores));
 
 //9
 //come back to
-const filterScores = (array, value) => {
-    return array.filter((obj) => {
-        return obj.name === value;
-    });
-};
-console.log(filterScores(scores, "Joe"));
+const filterScores = (array, property, value) => {
+    return array.filter(person => {
+        return person[property] === value;
+    })
+}
+//console.log(filterScores(scores, "passed"));
 
 //10. Declare a function named sortScoresAsc
 const sortScoresAsc = (array) => {
     return array.sort((a, b) => {
-        return a.score < b.score
+        return a.score < b.score ? 1 : -1
     });
 }
-console.log(sortScoresAsc(scores));
+//console.log(sortScoresAsc(scores));
+
+//11.
+const sortScoresDsc = (array) => {
+    return array.sort((a, b) => {
+        //b should come before
+        if (a.score > b.score) {
+            return -1;
+        } else if (a.score < b.score) {
+            return 1;
+        } else {
+            return 0;
+        }
+    })
+}
